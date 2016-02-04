e4336c6e8d2fb10ccdd82f7db870f985f9abe7f4
James Booth <james@teeny-tiny-mac-box.local>
2016-02-04 16:49:07 -0500

    Added all the button actions. Some comments added. Prelim code begins.

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -2,8 +2,10 @@
 //  ViewController.swift
 //  SimpleCalculator
 //
-//  Created by James Booth on 2016-02-04.
+//  Created by Jacqueline Richard on 2016-02-04.
 //  Copyright © 2016 Jacqueline Richard. All rights reserved.
+//  This app a simple calculator app that is suppose to allow the user to
+//  add, multiply, divide, and subtract two numbers.
 //
 
 import UIKit
@@ -19,7 +21,102 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+    //---------------------
+    //variables/constants
+    
+    
+    //---------------------
+    
+    //---------------------
+    //add all the click actions for the buttons
+    //clear the label and vairables
+    @IBAction func ClearButt(sender: UIButton) {
+    }
+    
+    //change a number to be positive or negative
+    @IBAction func posNegChange(sender: UIButton) {
+    }
+    
+    //add 0 to the num variable
+    @IBAction func numZero(sender: UIButton) {
+    }
+    
+    //add 1 to the num variable
+    @IBAction func numOne(sender: UIButton) {
+    }
+    
+    //add 2 to the variable
+    @IBAction func numTwo(sender: UIButton) {
+    }
+    
+    //add 3 to the variable
+    @IBAction func numThree(sender: UIButton) {
+    }
+    
+    //add 4 to the variable
+    @IBAction func numFour(sender: UIButton) {
+    }
+    
+    //add 5 to the variable
+    @IBAction func numFive(sender: UIButton) {
+    }
+    
+    //add 6 to the variable
+    @IBAction func numSix(sender: UIButton) {
+    }
+    
+    //add 7 to the variable
+    @IBAction func numSeven(sender: UIButton) {
+    }
+    
+    //add 8 to the variable
+    @IBAction func numEight(sender: UIButton) {
+    }
+    
+    //add 9 to the variable
+    @IBAction func numNine(sender: UIButton) {
+    }
+    
+    //add in a decimal
+    @IBAction func addDec(sender: UIButton) {
+        //check if decimal exists
+    }
+    
+    //get ready to divide
+    @IBAction func divide(sender: UIButton) {
+    }
+    
+    //get ready to multiply
+    @IBAction func multiply(sender: UIButton) {
+    }
+    
+    
+    //get ready to subtract
+    @IBAction func subtract(sender: UIButton) {
+    }
+    
+    //get ready to add
+    @IBAction func addition(sender: UIButton) {
+    }
+    
+    //equals button has been pressed. Time to do the math
+    @IBAction func doTheMath(sender: UIButton) {
+    }
+    
+    //--------------------
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}//end class
 
